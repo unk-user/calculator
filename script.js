6fa6651f02a3b465b5fac7ff328d51c1779032fc
Hamza <hamzalahmou01@gmail.com>
2023-10-15 23:56:36 +0100

    still needs minor improvements and +/- key functionality

---
+++ script.js
@@ -0,0 +1,117 @@
+const lastOperation = document.querySelector('#lastOperation');
+const currentOperation = document.querySelector('#currentOperation');
+const clearBtn = document.querySelector('#clear');
+const changeSign = document.querySelector('#changeSign');
+const number = document.querySelectorAll('#numbers');
+const operations = document.querySelectorAll('#operations');
+const dot = document.querySelector('#dot');
+const equals = document.querySelector('#equals');
+
+let a, b;
+let operation;
+let result;
+let index = 1;
+
+const appendNumber = (event) => {
+    if(currentOperation.textContent === '0'){
+        currentOperation.textContent = event.target.textContent;
+    } else {
+        currentOperation.textContent += event.target.textContent;
+    }
+}
+
+const appendOperation = (event) => {
+    if(lastOperation.textContent === ''){
+        operation = event.target.textContent;
+        a = Number(currentOperation.textContent);
+        lastOperation.textContent = currentOperation.textContent + operation;
+        currentOperation.textContent = '';
+        index = 0;
+    } else if(index === 0){
+        b = Number(currentOperation.textContent);
+        calculate(a, b, operation);
+        a = result;
+        operation = event.target.textContent;
+        lastOperation.textContent = `${result}` + operation;
+        currentOperation.textContent = '';
+    } else if(index === 1){
+        operation = event.target.textContent;
+        lastOperation.textContent = `${a}${operation}`;
+        currentOperation.textContent = '';
+    }
+}
+
+const addDot = () =>{
+    if((typeof Number(currentOperation.textContent.slice(-1))) === 'number'){
+        currentOperation.textContent += '.';
+    }
+}
+
+const calculate = (a, b, operation) => {
+    switch(operation){
+        case '÷':
+            return result = divide(a, b);
+            break;
+        case '×':
+            return result = multiply(a, b);
+            break;
+        case '-':
+            return result = substitute(a, b);
+            break;
+        case '+':
+            return result = add(a, b);
+            break;
+        case '%':
+            return result = percentage(a, b);
+            break;
+    }
+}
+
+const clear = () => {
+    currentOperation.textContent = '0';
+    lastOperation.textContent = ''
+}
+
+number.forEach(button => {
+    button.addEventListener('click', appendNumber);
+})
+
+operations.forEach(button => {
+    button.addEventListener('click', appendOperation);
+})
+
+equals.addEventListener('click', function (){
+    if(typeof a === 'number' || typeof b === 'number') {
+        b = Number(currentOperation.textContent);
+        calculate(a, b, operation);
+        lastOperation.textContent += currentOperation.textContent;
+        currentOperation.textContent = result;
+        a = result;
+        index = 1;
+    }
+});
+
+clearBtn.addEventListener('click', function () {
+    clear();
+    index = 1;
+});
+
+const add = (a, b) => {
+    return a + b;
+}
+const substitute = (a, b) => {
+    return a - b;
+}
+const multiply = (a, b) => {
+    return a * b;
+}
+const divide = (a, b) => {
+    if(b !== 0){
+        return Math.round((a / b) * 1000) / 1000;
+    } else {
+        return undefined;
+    }    
+}
+const percentage = (a, b) => {
+    return (a / b) * 100;
+}
